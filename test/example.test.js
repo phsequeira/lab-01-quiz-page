c096bbac02d0206bd1b216c2fd50087c6c5c975d
phsequeira <phsequeira@yahoo.com>
2021-01-11 13:58:03 -0800

    add test

--- test/example.test.js
+++ test/example.test.js
@@ -3,14 +3,28 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('to check if the first char of checkIfYes is a Y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = checkIfYes('yes');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('to check if the first char of checkIfYes is a not a Y', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = checkIfYes('no');
 
     //Expect
     // Make assertions about what is expected versus the actual result
