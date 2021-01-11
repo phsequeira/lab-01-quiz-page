d6110331dcdf0e6f9ac21cd84cc8c1876f751dff
phsequeira <phsequeira@yahoo.com>
2021-01-11 15:02:42 -0800

    add comfirm test and first name promt

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // import functions and grab DOM elements
 
 // initialize state
-
+const quizButton = document.getElementById('startQuiz');
+let correctAnswers = 0;
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+    //promt alert
+    alert('Welcome to the WSL Quiz!');
+    //promt confirm
+    const readyForTest = confirm('Are you ready to test your knowledge?');
+
+    if (!readyForTest) return;
+
+    const firstName = prompt('Please enter your first name.');
+    console.log(firstName);
+
+   
+  
+});
+
